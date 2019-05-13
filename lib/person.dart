ace37eeea9fd70a15b409006070816c7a92adda9
Matheus Torreão <matheus.torreao@avanade.com>
2019-05-13 13:46:13 -0300

    Melhoria no model de Person e salvamento do formulario de person

--- lib/person.dart
+++ lib/person.dart
@@ -1,13 +1,27 @@
 class Person {
   String firstName;
   String fullName;
+  int cellphone1;
 
   Person.fromSnapshot(document) {
     this.firstName = document['first_name'];
+    this.fullName = document['full_name'];
+    this.cellphone1 = document['cellphone_1'];
   }
+
+  Person({this.firstName, this.fullName, this.cellphone1});
 
   @override
   String toString() {
-    return 'First Name: $firstName';
+    return 'First Name: $firstName\nCellphone 1: $cellphone1';
+  }
+
+  Map<String, dynamic> toMap() {
+    firstName = this.fullName.split(' ')[0];
+    return Map.from({
+      'first_name': this.firstName,
+      'full_name': this.fullName,
+      'cellphone_1': this.cellphone1
+    });
   }
 }
--- lib/person_form.dart
+++ lib/person_form.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dosie_app/auth_service.dart';
+import 'package:dosie_app/person.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PersonForm extends StatefulWidget {
@@ -6,6 +10,8 @@ class PersonForm extends StatefulWidget {
 }
 
 class _PersonFormState extends State<PersonForm> {
+  final _person = Person();
+
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   final _formKey = GlobalKey<FormState>();
 
@@ -14,19 +20,24 @@ class _PersonFormState extends State<PersonForm> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('DEF'),
+        title: Text('Pessoa'),
       ),
       body: Padding(
         padding:
-            EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0, bottom: 16.0),
+        EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0, bottom: 16.0),
         child: Form(
           key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.only(top: 8.0,),
-                child: Text('Dados Pessoais', style: TextStyle(fontSize: 20.0),),
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                ),
+                child: Text(
+                  'Dados Pessoais',
+                  style: TextStyle(fontSize: 20.0),
+                ),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Nome Completo'),
@@ -37,35 +48,57 @@ class _PersonFormState extends State<PersonForm> {
                     return 'Colocar nome e sobrenome';
                   }
                 },
+                onSaved: (value) {
+                  _person.fullName = value;
+                },
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 8.0,),
-                child: Text('Contatos', style: TextStyle(fontSize: 20.0),),
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                ),
+                child: Text(
+                  'Contatos',
+                  style: TextStyle(fontSize: 20.0),
+                ),
               ),
               TextFormField(
+                keyboardType: TextInputType.numberWithOptions(decimal: false),
                 decoration: InputDecoration(labelText: 'Telefone 1'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Não pode ser vazio';
+//                  } else if () {
+//                    return 'Não é um número de telefone válido.';
+                  }
+                },
+                onSaved: (value) {
+                  _person.cellphone1 = int.parse(value);
+                },
               ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Telefone 2'),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Text('Gostos', style: TextStyle(fontSize: 20.0)),
-              ),
+//              Padding(
+//                padding: const EdgeInsets.only(top: 8.0),
+//                child: Text('Gosta de', style: TextStyle(fontSize: 20.0)),
+//              ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_formKey.currentState.validate()) {
-            print('Validado com sucesso!');
-            _scaffoldKey.currentState
-                .showSnackBar(SnackBar(content: Text('Validado com sucesso!')));
-          }
-        },
+        onPressed: _saveForm,
         child: Icon(Icons.save),
       ),
     );
+  }
+
+  Future _saveForm() async {
+    if (_formKey.currentState.validate()) {
+      print('Validado com sucesso!');
+      _formKey.currentState.save();
+      FirebaseUser user = await AuthService().getUser();
+      Firestore.instance.collection('dosie_app/${user.uid}/people').add(
+          _person.toMap());
+      _scaffoldKey.currentState
+          .showSnackBar(SnackBar(content: Text('Validado com sucesso!')));
+    }
   }
 }
